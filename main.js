6271db391e22af55889e80ef098c60b9c8188732
CyberSec77 <kevin@cityforum.com>
2021-01-22 10:47:31 -0500

    Adding Box Information

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+// Typing effect in Javascript
 const typedTextSpan = document.querySelector(".typing1");
 
+// Things displayed after the initial text
 const textArray = ["a 18 year old high student", "a freelance web developer", "a creator"];
 
 const typingDelay = 200;
@@ -12,6 +14,7 @@ let textArrayIndex = 0;
 
 let charIndex = 0;
 
+// Types the things in the website
 function type() {
     if(charIndex < textArray[textArrayIndex].length) {
 typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
@@ -23,6 +26,7 @@ setTimeout(type, typingDelay);
     }
 }
 
+// Erases the things typed in the website
 function erase() {
     if(charIndex > 0) {
         typedTextSpan.textContent = textArray[textArrayIndex].substring(0,charIndex-1);
